6d1d7fc6e158028af248b10faf37a0b60036bea8
yadav vinit <vinitya013@gmail.com>
2026-04-29 20:46:09 +0530

    user-define-list

---
+++ user define list/Remove-Elements.py
@@ -0,0 +1,5 @@
+n = int(input("enter element no"))
+lst = [1, 2, 3, 4, 5]
+lst.remove(n)
+lst.pop()
+print(lst)
---
+++ user define list/start-element-ratio-list.py
@@ -0,0 +1,11 @@
+thislist = []
+n = int(input("Enter number of items: "))
+
+for i in range(n):
+    item = input("Enter item: ")
+    thislist.append(item)
+
+start = int(input("Enter starting index: "))
+
+print("Full list:", thislist)
+print("After index", start, ":", thislist[start:])
