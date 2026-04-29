13072e8e0cb1e79a3586e5f66e625dcec3ba9ead
yadav vinit <vinitya013@gmail.com>
2026-04-29 20:39:40 +0530

    upload

--- list/start-element-ratio-list.py
+++ list/start-element-ratio-list.py
@@ -1,3 +1,11 @@
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:])
+thislist = []
+n = int(input("Enter number of items: "))
 
+for i in range(n):
+    item = input("Enter item: ")
+    thislist.append(item)
+
+start = int(input("Enter starting index: "))
+
+print("Full list:", thislist)
+print("After index", start, ":", thislist[start:])
